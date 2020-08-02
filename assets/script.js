edc58f8bc0de8320dcef0f3e619ed5503f6d4f91
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-08-02 13:52:55 -0500

    pseudocode created for js

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    //show day and time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    
+})
+// save button click event handlers: when button is clicked, value of description is saved
+
+// set items to loca; storage
+
+// conditional statements to create different colors for current, past, and future, time blocks
+
+// loop through each hour: select each hour and loop
+// loop through each hour element dibv that is storing time and check against current time
+
+// textArea.addClass("present")
+
+// parse hours into a time (in documentation)
+// for loops, use .each
+// when hour selected use .each function to run function on each element that matches jquery selector
+
+
+// save descriptions of time blocks to local storage to stay even after refreshing page
