3e5c684ca8cdbf212201d4003d038f834049d9aa
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-08-03 18:38:57 -0500

    changed to 24 hour format

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,64 @@
 $(document).ready(function () {
     //show current day and time
-   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    $("#currentDay").text(moment().format("MMMM Do YYYY, hh:mm:ss a"));
 
     // save button click event: when button is clicked, description saves
-    $(".saveBtn").on("click", function(){
-        
+    $(".saveBtn").on("click", function () {
+
         var description = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         // set items to local storage
         localStorage.setItem(time, description);
-    })
-    
-    // save/load descriptions of time blocks on local storage to stay even after refreshing page
-    $("#9am .description").val(localStorage.getItem("9AM"));
-    $("#10am .description").val(localStorage.getItem("10AM"));
-    $("#11am .description").val(localStorage.getItem("11AM"));
-    $("#12pm .description").val(localStorage.getItem("12PM"));
-    $("#1pm .description").val(localStorage.getItem("1PM"));
-    $("#2pm .description").val(localStorage.getItem("2PM"));
-    $("#3pm .description").val(localStorage.getItem("3PM"));
-    $("#4pm .description").val(localStorage.getItem("4PM"));
-    $("#5pm .description").val(localStorage.getItem("5PM"));
-    $("#6pm .description").val(localStorage.getItem("6PM"));
+        console.log(localStorage);
+    });
 
+    // save/load descriptions of time blocks on local storage to stay even after refreshing page
+    
+    function getItems() {
+    $("#hour-9 .description").val(localStorage.getItem("9AM"));
+    $("#hour-10 .description").val(localStorage.getItem("10AM"));
+    $("#hour-11 .description").val(localStorage.getItem("11AM"));
+    $("#hour-12 .description").val(localStorage.getItem("12PM"));
+    $("#hour-13 .description").val(localStorage.getItem("1PM"));
+    $("#hour-14 .description").val(localStorage.getItem("2PM"));
+    $("#hour-15 .description").val(localStorage.getItem("3PM"));
+    $("#hour-16 .description").val(localStorage.getItem("4PM"));
+    $("#hour-17 .description").val(localStorage.getItem("5PM"));
+    $("#hour-18 .description").val(localStorage.getItem("6PM"));
+    }
+    
 
     // timekeeper/tracker function here
     function timeTracker() {
         var currentHour = moment().hour();
 
         // loop through each hour: select each hour and loop
-        $(".hour-block").each(function () {
-            
-            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
-            console.log(currentHour, timeBlock); //not sure why this is nAn
-            
+        $(".time-block").each(function () {
+
+            var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(currentHour, hourBlock); 
+
 
             // statements for moving past the time blocks to make them different colors
-            if (timeBlock < currentHour) {
-                textArea.addClass("past");
-                textArea.removeClass("present");
-                textArea.removeClass("future");
-            }; 
-            if (timeBlock > currentHour) {
-                textArea.removeClass("past");
-                textArea.removeClass("present");
-                textArea.addClass("future");
+            if (hourBlock < currentHour) {
+                $(this).addClass("past");
+               
             }
-                
-                if  (timeBlock === currentHour) {
-                    textArea.removeClass("past");
-                    textArea.addClass("present");
-                    textArea.removeClass("future");
-                    }
+            else if (hourBlock === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
 
-            })
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+
+        })
     }
-
+        getItems();
     timeTracker();
 });
 
@@ -66,15 +68,5 @@ $(document).ready(function () {
 
 
 
-// conditional statements to create different colors for current, past, and future, time blocks
-
-
-
-
-// textArea.addClass("present")
-
-// parse hours into a time (in documentation)
-// for loops, use .each
-// when hour selected use .each function to run function on each element that matches jquery selector
 
 
